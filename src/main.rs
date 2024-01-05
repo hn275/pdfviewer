2aa06f7b9a99ce44963779841e45d0b66b34914b
Hal <haln_01@proton.me>
2024-01-04 22:37:33 -0800

    feat: watch file done

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,74 @@
 use futures_util::SinkExt;
-use notify::{self, Watcher};
-use std::{path::Path, sync::mpsc, thread};
+use std::{env, fmt, fs, process, thread::sleep, time::Duration};
 use warp::{filters::ws::WebSocket, ws, Filter};
 
-struct Reloaded;
-
 #[tokio::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
+    // parse arg
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() != 2 {
+        error_exit("Invalid number of args.");
+    }
+    let path = args[1].as_str();
+    // file exists?
+    fs::metadata(path).expect("File not found.");
+    println!("Watching: {}", path);
+
+    // connection
     let routes = warp::path("viewer")
-        // The `ws()` filter will prepare the Websocket handshake.
-        .and(warp::ws())
-        .map(move |ws: ws::Ws| {
-            let (tx, rx) = mpsc::channel::<Reloaded>();
-
-            // file watch initialization
-            thread::spawn(move || {
-                notify::RecommendedWatcher::new(
-                    move |event: Result<notify::Event, notify::Error>| {
-                        let event = event.unwrap();
-                        println!("event emitted");
-                        dbg!(&event);
-                        tx.send(Reloaded {}).unwrap();
-                        use notify::EventKind;
-                        match event.kind {
-                            EventKind::Modify(msg) => {
-                                dbg!(&msg);
-                            }
-                            _ => {
-                                eprintln!("Error");
-                            }
-                        };
-                    },
-                    notify::Config::default(),
-                )
-                .unwrap()
-                .watch(
-                    Path::new("/home/haln/codes/typst-viewer/test.json"),
-                    notify::RecursiveMode::Recursive,
-                )
-                .unwrap();
-            });
-
-            // websocket handling
-            ws.on_upgrade(move |websocket| handle_connection(websocket, rx))
-        });
+        .and(warp::ws()) // The `ws()` filter will prepare the Websocket handshake.
+        .map(move |ws: ws::Ws| ws.on_upgrade(move |websocket| handle_connection(websocket)));
 
     warp::serve(routes).run(([127, 0, 0, 1], 8081)).await;
+    return Ok(());
 }
 
-async fn handle_connection(mut websocket: WebSocket, rx: mpsc::Receiver<Reloaded>) {
-    loop {
-        while rx.recv().is_ok() {
-            websocket
-                .send(ws::Message::text("Hello world"))
-                .await
-                .unwrap();
+async fn handle_connection(mut websocket: WebSocket) {
+    // NOTE: since at the file has exists for this function to be called,
+    // `unwrap()` is ok for file existence
 
-            websocket
-                .send(ws::Message::binary("Hey ho".as_bytes())) // this sends it as blob
-                .await
-                .unwrap();
+    // parse arg
+    let args = env::args().collect::<Vec<String>>();
+    let path = args[1].as_str();
+
+    // read file
+    let mut last_modified = fs::metadata(path)
+        .unwrap()
+        .modified()
+        .expect("Platform not supported");
+
+    let file = fs::read(path).unwrap();
+    if let Err(err) = websocket.send(ws::Message::binary(file)).await {
+        error_exit(err);
+        return;
+    }
+
+    // pooling for file changes
+    loop {
+        let file_metadata = fs::metadata(path).unwrap();
+        let modified_at = file_metadata.modified().expect("Platform is not supported");
+
+        // no change detected
+        if modified_at == last_modified {
+            sleep(Duration::from_millis(100)); // TODO: add cli args for this timeout, default to 100 ms
+            continue;
+        }
+
+        // file changed
+        // TODO: add a -v tag to print out logs like this
+        println!("changes detected.");
+        last_modified = modified_at;
+
+        // send data
+        let file = fs::read(path).expect("File not found.");
+        if let Err(err) = websocket.send(ws::Message::binary(file)).await {
+            error_exit(err);
+            return;
         }
     }
+}
+
+fn error_exit(err: impl fmt::Display) {
+    eprintln!("{}", err);
+    process::exit(1);
 }
