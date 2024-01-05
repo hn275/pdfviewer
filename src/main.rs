56e01cd85ba4c137c23531c478ab6db8d1f0014e
Hal <haln_01@proton.me>
2024-01-05 12:57:47 -0800

    feat: axum

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,73 @@
+use axum::{
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 use futures_util::SinkExt;
-use std::{env, fmt, fs, process, thread, time::Duration};
-use warp::{filters::ws::WebSocket, ws, Filter};
+use std::{
+    env, fmt, fs, process,
+    sync::{self, Arc},
+    thread,
+    time::Duration,
+};
+
+#[derive(Clone)]
+struct Reloaded;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() {
     // parse arg
     let args = env::args().collect::<Vec<String>>();
     if args.len() != 2 {
         error_exit("Invalid number of args.");
     }
-    let path = args[1].as_str();
+    let path = args[1].to_owned();
     // file exists?
-    fs::metadata(path).expect("File not found.");
+    fs::metadata(path.as_str()).expect("File not found.");
     println!("Watching: {}", path);
 
+    // watch file
+    let (tx, rx) = sync::mpsc::sync_channel::<u8>(1);
+    thread::spawn(move || {
+        let mut last_modified = fs::metadata(path.as_str())
+            .expect("File not found.")
+            .modified()
+            .expect("Platform not supported.");
+        loop {
+            thread::sleep(Duration::from_millis(300));
+            let file_modified = fs::metadata(path.as_str())
+                .expect("File not found.")
+                .modified()
+                .expect("Platform not supported.");
+            if file_modified == last_modified {
+                continue;
+            }
+
+            // file changed
+            // TODO: add a -v tag to print out logs like this
+            println!("changes detected.");
+            last_modified = file_modified;
+
+            // send signal
+            tx.send(1).expect("Failed to send signal");
+        }
+    });
+
+    let rx = Arc::new(rx);
     // connection
-    let routes = warp::path("viewer")
-        .and(warp::ws()) // The `ws()` filter will prepare the Websocket handshake.
-        .map(move |ws: ws::Ws| ws.on_upgrade(move |websocket| handle_connection(websocket)));
-
-    // TODO: use channel here to watch file so only one watch loop is needed for multiple connections
-
-    warp::serve(routes).run(([127, 0, 0, 1], 8081)).await;
-    return Ok(());
+    let app = Router::new()
+        .route("/viewer", get(handle_connection))
+        .with_state(Arc::clone(&rx));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8081").await.unwrap();
+    return axum::serve(listener, app).await.unwrap();
 }
 
-async fn handle_connection(mut websocket: WebSocket) {
+async fn handle_connection(ws: WebSocketUpgrade) -> Response {
+    return ws.on_upgrade(handle_socket);
+}
+
+async fn handle_socket(mut socket: WebSocket) {
     println!("New connection detected!");
     // NOTE: since at the file has exists for this function to be called,
     // `unwrap()` is ok for file existence
@@ -41,33 +83,9 @@ async fn handle_connection(mut websocket: WebSocket) {
         .expect("Platform not supported");
 
     let file = fs::read(path).unwrap();
-    if let Err(err) = websocket.send(ws::Message::binary(file)).await {
+    if let Err(err) = socket.send(Message::Binary(file)).await {
         error_exit(err);
         return;
-    }
-
-    // pooling for file changes
-    loop {
-        thread::sleep(Duration::from_millis(500)); // TODO: add cli args for this timeout, default to 100 ms
-        let file_metadata = fs::metadata(path).unwrap();
-        let modified_at = file_metadata.modified().expect("Platform is not supported");
-
-        // no change detected
-        if modified_at == last_modified {
-            continue;
-        }
-
-        // file changed
-        // TODO: add a -v tag to print out logs like this
-        println!("changes detected.");
-        last_modified = modified_at;
-
-        // send data
-        let file = fs::read(path).expect("File not found.");
-        if let Err(err) = websocket.send(ws::Message::binary(file)).await {
-            error_exit(err);
-            return;
-        }
     }
 }
 
